091d87c412ee5c91b773c4e1cfa7a111e9e7f083
길재우 <6197@.>
2020-12-18 18:51:31 +0900

    test commint

--- goldrithm/src/main/java/com/goldrithm/settingweb/HomeController.java
+++ goldrithm/src/main/java/com/goldrithm/settingweb/HomeController.java
@@ -2,8 +2,8 @@ package com.goldrithm.settingweb;
 
 import com.goldrithm.service.MovieService;
 import com.goldrithm.vo.MovieVO;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +19,7 @@ import java.util.Locale;
 @Controller
 public class HomeController {
 
-    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+//    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @Inject
     private MovieService service;
@@ -30,7 +30,7 @@ public class HomeController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(Locale locale, Model model) throws Exception{
 
-        logger.info("home");
+//        logger.info("home");
         System.out.println("서비스 진입전");
         List<MovieVO> movieList = service.selectMovie();
         System.out.println("서비스 완료후");
