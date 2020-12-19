e342b4a988fc5ccd653a81eb2491ea1a941f1cdf
길재우 <kh6197@naver.com>
2020-12-19 18:43:10 +0900

    log4j2를 사용하여 로그 띄움.

--- goldrithm/src/main/java/com/goldrithm/settingweb/HomeController.java
+++ goldrithm/src/main/java/com/goldrithm/settingweb/HomeController.java
@@ -1,17 +1,20 @@
 package com.goldrithm.settingweb;
 
-import com.goldrithm.service.MovieService;
-import com.goldrithm.vo.MovieVO;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
+import java.util.List;
+import java.util.Locale;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.inject.Inject;
-import java.util.List;
-import java.util.Locale;
+import com.goldrithm.service.MovieService;
+import com.goldrithm.vo.MovieVO;
+
 
 /**
  * Handles requests for the application home page.
@@ -19,7 +22,7 @@ import java.util.Locale;
 @Controller
 public class HomeController {
 
-//    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @Inject
     private MovieService service;
@@ -30,7 +33,7 @@ public class HomeController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(Locale locale, Model model) throws Exception{
 
-//        logger.info("home");
+        logger.info("home");
         System.out.println("서비스 진입전");
         List<MovieVO> movieList = service.selectMovie();
         System.out.println("서비스 완료후");
